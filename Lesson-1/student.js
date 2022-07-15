58f0e443e0fe5d6c2c08822a8e324ec8bf2b2b59
MD.Kamrul Hasan <kamrulhasan635653@gmail.com>
2022-07-15 23:34:53 +0600

    first commit

---
+++ Lesson-1/student.js
@@ -0,0 +1,19 @@
+const getName = () => {
+  return "MD.Kamrul Hasan";
+};
+
+const getAge = () => {
+  return "25";
+};
+
+const cgpa = 3.92;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+module.exports = {
+  getName,
+  getAge,
+  cgpa,
+};
